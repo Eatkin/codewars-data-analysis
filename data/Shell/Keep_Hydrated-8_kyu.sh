f4d993ec141b1236805788254c9e863f45d49bc3
Edward Atkin <edward.atkin@live.com>
2023-06-14 07:06:23 +0100

    Update data set. Analyse individual problems and most popular authors.

--- data/Shell/Keep_Hydrated-8_kyu.sh
+++ data/Shell/Keep_Hydrated-8_kyu.sh
@@ -1,4 +1,6 @@
 # https://www.codewars.com/kata/582cb0224e56e068d800003c
-# 2023-04-17T18:06:43.289+0000
-def litres(time):
-    return (time * 0.5)//1
+# 2023-04-08T14:49:00.663+0000
+#!/bin/bash
+time=$1
+# The fun begins here.
+echo "$(echo "scale=0; $time*0.5/1" | bc)"
